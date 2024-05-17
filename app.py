de0610e99ca8fa3f87f52c4f40d98a85b24d1fd3
Chandan Vasu <chandanyadavparas@gmail.com>
2024-05-17 17:50:14 +0530

    main code

--- app.py
+++ app.py
@@ -2,19 +2,9 @@ import json
 from flask import Flask, request, Response
 import requests
 from rembg import remove
-import os
 
 app = Flask(__name__)
 
-# Get the port from the PORT environment variable or use 8080 as default
-port = int(os.environ.get('PORT', 8080))
-
-# Health check route
-@app.route('/', methods=['GET'])
-def health_check():
-    return Response(json.dumps({'status': 'OK'}), status=200, mimetype='application/json')
-
-# Background removal route
 @app.route('/remove_background', methods=['GET'])
 def remove_background():
     # Extract the URL parameter from the query string
@@ -34,5 +24,4 @@ def remove_background():
     return Response(output_data, status=200, mimetype='image/png')
 
 if __name__ == '__main__':
-    # Run the app with binding to the port specified by the PORT environment variable or 8080 as default
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=True)
